689fa688f7bbd58fe00877f29e9f7f60a68aff97
MoSuperCode <mo.supercode@gmail.com>
2025-05-25 20:43:35 +0200

    some more debugging

--- src/main/java/org/example/tourplannerbackend/TourplannerBackendApplication.java
+++ src/main/java/org/example/tourplannerbackend/TourplannerBackendApplication.java
@@ -2,12 +2,12 @@ package org.example.tourplannerbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.client.HttpClientAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {HttpClientAutoConfiguration.class})
 public class TourplannerBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TourplannerBackendApplication.class, args);
     }
-
 }
