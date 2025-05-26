b9c9fc417d0f42802f3d22a9f7a116331628b546
MoSuperCode <mo.supercode@gmail.com>
2025-05-26 12:30:57 +0200

    Backend successfully debugged

--- src/main/java/org/example/tourplannerbackend/TourplannerBackendApplication.java
+++ src/main/java/org/example/tourplannerbackend/TourplannerBackendApplication.java
@@ -2,9 +2,8 @@ package org.example.tourplannerbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.http.client.HttpClientAutoConfiguration;
 
-@SpringBootApplication(exclude = {HttpClientAutoConfiguration.class})
+@SpringBootApplication
 public class TourplannerBackendApplication {
 
     public static void main(String[] args) {
